7f09e983418d99554e2c45a2bf34301eb71eb43e
Jeremy Hirsch <jeremy_hirsch@live.com>
2019-01-16 12:51:02 -0500

    no auth needed for /tag methods that are GET methods

--- src/main/java/gov/bnl/channelfinder/WebSecurityConfig.java
+++ src/main/java/gov/bnl/channelfinder/WebSecurityConfig.java
@@ -1,13 +1,22 @@
 package gov.bnl.channelfinder;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web
+	        .ignoring()
+	        .antMatchers(HttpMethod.GET, "/tag**");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
