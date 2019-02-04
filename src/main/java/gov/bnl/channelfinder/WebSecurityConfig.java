a82d558665e53f5cd2f1739d1ccc4324a02fa613
Kunal Shroff <kunalshroff9@gmail.com>
2019-02-04 10:10:06 -0500

    allow all GET operations without AA

--- src/main/java/gov/bnl/channelfinder/WebSecurityConfig.java
+++ src/main/java/gov/bnl/channelfinder/WebSecurityConfig.java
@@ -13,41 +13,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-	    web
-	        .ignoring()
-	        .antMatchers(HttpMethod.GET, "/tag**");
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated();
-		http.httpBasic();
-		http.formLogin().permitAll().and().logout().logoutSuccessUrl("/");
-	}
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.GET, "/**");
+    }
 
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.ldapAuthentication()
-				.userDnPatterns("uid={0},ou=people")
-				.groupSearchBase("ou=groups")
-				.contextSource()
-				.url("ldap://localhost:8389/dc=springframework,dc=org")
-				//.url("ldap://localhost:8080")
-				.and()
-				.passwordCompare()
-					.passwordEncoder(new LdapShaPasswordEncoder())
-					.passwordAttribute("userPassword");
-		auth.inMemoryAuthentication()
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic();
+        http.formLogin().permitAll().and().logout().logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                // .url("ldap://localhost:8080")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+        auth.inMemoryAuthentication()
         .withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN")
         .and()
-        .withUser("user").password(encoder().encode("userPass")).roles("USER");
-	}
-	
-	@Bean
-	public PasswordEncoder  encoder() {
-	    return new BCryptPasswordEncoder();
-	}
-	
+                .withUser("user").password(encoder().encode("userPass")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
