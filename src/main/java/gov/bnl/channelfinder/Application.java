faca5288eb34f2c2a4e4ec72ea896d704c443e33
Kunal Shroff <kunalshroff9@gmail.com>
2019-05-08 13:47:02 -0400

    Fixing application ssl configuration to work with spring and jar

--- src/main/java/gov/bnl/channelfinder/Application.java
+++ src/main/java/gov/bnl/channelfinder/Application.java
@@ -1,5 +1,3 @@
-package gov.bnl.channelfinder;
-
 /*
  * #%L
  * ChannelFinder Directory Service
@@ -10,23 +8,55 @@ package gov.bnl.channelfinder;
  * All rights reserved. Use is subject to license terms.
  * #L%
  */
-//package edu.msu.nscl.olog;
+package gov.bnl.channelfinder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.FileCopyUtils;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages="gov.bnl.channelfinder")
 @SpringBootApplication
 public class Application {
+
+    static Logger logger = Logger.getLogger(Application.class.getName());
+
     public static void main(String[] args) {
         // Set the java truststore used by channelfinder
-        System.setProperty("javax.net.ssl.trustStore", Application.class.getResource("/keystore/cacerts").getPath());
-        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
-
+        configureTruststore();
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Set the default ssl trust store
+     */
+    private static void configureTruststore() {
+        if (System.getProperty("javax.net.ssl.trustStore") == null) {
+            logger.log(Level.INFO, "using default javax.net.ssl.trustStore");
+            try (InputStream in = Application.class.getResourceAsStream("/keystore/cacerts")) {
+                // read input
+                File tempFile= File.createTempFile("cf-", "-truststore");
+                FileOutputStream out = new FileOutputStream(tempFile);
+                FileCopyUtils.copy(in, out);
+                tempFile.deleteOnExit();
+                System.setProperty("javax.net.ssl.trustStore", tempFile.getAbsolutePath());
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "failed to configure channelfinder truststore", e);
+            }
+        }
+        if (System.getProperty("javax.net.ssl.trustStorePassword") == null) {
+            logger.log(Level.INFO, "using default javax.net.ssl.trustStorePassword");
+            System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+        }
+        
     }
 
 }
