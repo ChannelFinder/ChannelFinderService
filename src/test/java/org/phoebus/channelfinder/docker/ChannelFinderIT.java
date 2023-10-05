224ef9ba862c645c6140476c5f5472b402b6aade
Lars Johansson <lars.johansson@ess.eu>
2023-10-05 14:25:22 +0200

    Add code coverage for integration tests with Docker
    Add code coverage for integration tests with Docker.
    By default, code coverage is disabled.

--- src/test/java/org/phoebus/channelfinder/docker/ChannelFinderIT.java
+++ src/test/java/org/phoebus/channelfinder/docker/ChannelFinderIT.java
@@ -18,13 +18,18 @@
 
 package org.phoebus.channelfinder.docker;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.ContainerState;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.github.dockerjava.api.DockerClient;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -58,6 +63,28 @@ class ChannelFinderIT {
 
 	@Container
     public static final ComposeContainer ENVIRONMENT = ITUtil.defaultComposeContainers();
+
+    @AfterAll
+    public static void extractJacocoReport() {
+        // extract jacoco report from container file system
+        //     stop jvm to make data available
+
+        if (!Boolean.FALSE.toString().equals(System.getProperty(ITUtil.JACOCO_SKIPITCOVERAGE))) {
+            return;
+        }
+
+        Optional<ContainerState> container = ENVIRONMENT.getContainerByServiceName(ITUtil.CHANNELFINDER);
+        if (container.isPresent()) {
+            ContainerState cs = container.get();
+            DockerClient dc = cs.getDockerClient();
+            dc.stopContainerCmd(cs.getContainerId()).exec();
+            try {
+                cs.copyFileFromContainer(ITUtil.JACOCO_EXEC_PATH, ITUtil.JACOCO_TARGET_PREFIX + ChannelFinderIT.class.getSimpleName() + ITUtil.JACOCO_TARGET_SUFFIX);
+            } catch (Exception e) {
+                // proceed if file cannot be copied
+            }
+        }
+    }
 
     @Test
     void channelfinderUp() {
