92f1bda6c080a98884a9ae65072f3442cbe3390d
Sky Brewer <sky.brewer@ess.eu>
2023-12-07 16:21:08 +0100

    Update the web security config for spring boot 3

--- src/main/java/org/phoebus/channelfinder/WebSecurityConfig.java
+++ src/main/java/org/phoebus/channelfinder/WebSecurityConfig.java
@@ -1,34 +1,51 @@
 package org.phoebus.channelfinder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.PersonContextMapper;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WebSecurityConfig  {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().anyRequest().authenticated();
         http.httpBasic();
+        return http.build();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
         // Authentication and Authorization is only needed for non search/query operations
-        web.ignoring().antMatchers(HttpMethod.GET, "/**");
+        return (web) -> web.ignoring().antMatchers(HttpMethod.GET, "/**");
     }
 
     /**
@@ -79,67 +96,73 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${file.auth.enabled:true}")
     boolean file_enabled;
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
+        EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
+                EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
+        contextSourceFactoryBean.setPort(0);
+        return contextSourceFactoryBean;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) {
 
         if (ldap_enabled) {
             DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource(ldap_url);
             contextSource.afterPropertiesSet();
-
             DefaultLdapAuthoritiesPopulator myAuthPopulator = new DefaultLdapAuthoritiesPopulator(contextSource, ldap_groups_search_base);
             myAuthPopulator.setGroupSearchFilter(ldap_groups_search_pattern);
             myAuthPopulator.setSearchSubtree(true);
             myAuthPopulator.setIgnorePartialResultException(true);
 
-            auth.ldapAuthentication()
-                .userDnPatterns(ldap_user_dn_pattern)
-                .ldapAuthoritiesPopulator(myAuthPopulator)
-                .contextSource(contextSource);
+            LdapBindAuthenticationManagerFactory factory =
+                    new LdapBindAuthenticationManagerFactory(contextSource);
+            factory.setLdapAuthoritiesPopulator(myAuthPopulator);
+            factory.setUserDnPatterns(ldap_user_dn_pattern);
+            factory.setUserDetailsContextMapper(new PersonContextMapper());
+            return factory.createAuthenticationManager();
         }
 
         if (embedded_ldap_enabled) {
             DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource(embedded_ldap_url);
             contextSource.afterPropertiesSet();
-
             DefaultLdapAuthoritiesPopulator myAuthPopulator = new DefaultLdapAuthoritiesPopulator(contextSource, embedded_ldap_groups_search_base);
             myAuthPopulator.setGroupSearchFilter(embedded_ldap_groups_search_pattern);
             myAuthPopulator.setSearchSubtree(true);
             myAuthPopulator.setIgnorePartialResultException(true);
 
-
-            auth.ldapAuthentication()
-                    .userDnPatterns(embedded_ldap_user_dn_pattern)
-                    .ldapAuthoritiesPopulator(myAuthPopulator)
-                    .groupSearchBase("ou=Group")
-                    .contextSource(contextSource);
+            LdapBindAuthenticationManagerFactory factory =
+                    new LdapBindAuthenticationManagerFactory(contextSource);
+            factory.setLdapAuthoritiesPopulator(myAuthPopulator);
+            factory.setUserDnPatterns(embedded_ldap_user_dn_pattern);
+            factory.setUserSearchBase("ou=Group");
+            factory.setUserDetailsContextMapper(new PersonContextMapper());
+            return factory.createAuthenticationManager();
 
         }
+        return null;
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
 
-        if (demo_auth_enabled) {
-            // read from configuration, no default content
-            //     interpret users, pwds, roles
-            //     user may have multiple roles
+        if (demo_auth_enabled &&  (demo_auth_users != null
+                && demo_auth_pwds != null
+                && demo_auth_roles != null
+                && demo_auth_users.length > 0
+                && demo_auth_users.length == demo_auth_pwds.length
+                && demo_auth_pwds.length == demo_auth_roles.length)) {
 
-            if (demo_auth_users != null
-                    && demo_auth_pwds != null
-                    && demo_auth_roles != null
-                    && demo_auth_users.length > 0
-                    && demo_auth_users.length == demo_auth_pwds.length
-                    && demo_auth_pwds.length == demo_auth_roles.length) {
-
-                for (int i=0; i<demo_auth_users.length; i++) {
-                    String[] userroles = demo_auth_roles[i].split(demo_auth_delimiter_roles);
-                    if (userroles != null && userroles.length > 0) {
-                        auth.inMemoryAuthentication()
-                                .withUser(demo_auth_users[i])
-                                .password(encoder().encode(demo_auth_pwds[i]))
-                                .roles(userroles);
-                    }
+            List<UserDetails> users = new ArrayList<>();
+            for (int i=0; i<demo_auth_users.length; i++) {
+                String[] userroles = demo_auth_roles[i].split(demo_auth_delimiter_roles);
+                if (userroles.length > 0) {
+                    users.add(User.withUsername(demo_auth_users[i]).password(encoder().encode(demo_auth_pwds[i])).roles(userroles).build());
                 }
             }
-        }
-    }
+            return new InMemoryUserDetailsManager(users);
 
+        }
+        return null;
+    }
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
