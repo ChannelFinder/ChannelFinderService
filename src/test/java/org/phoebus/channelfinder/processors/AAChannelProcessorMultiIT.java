a252f5d57a9ba94c6ce00b9ed64478edf2787129
Sky Brewer <sky.brewer@ess.eu>
2024-09-02 16:49:42 +0200

    Test sending to archiver 99 PVs

--- src/test/java/org/phoebus/channelfinder/processors/AAChannelProcessorMultiIT.java
+++ src/test/java/org/phoebus/channelfinder/processors/AAChannelProcessorMultiIT.java
@@ -8,6 +8,9 @@ import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.phoebus.channelfinder.entity.Channel;
 import org.phoebus.channelfinder.processors.aa.AAChannelProcessor;
 import org.phoebus.channelfinder.processors.aa.ArchiveAction;
@@ -22,6 +25,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -33,6 +39,10 @@ import static org.phoebus.channelfinder.processors.AAChannelProcessorIT.inactive
 @TestPropertySource(value = "classpath:application_test.properties")
 class AAChannelProcessorMultiIT {
 
+    public static final String BEING_ARCHIVED = "Being archived";
+    public static final String PAUSED = "Paused";
+    public static final String NOT_BEING_ARCHIVED = "Not being archived";
+    public static final String OWNER = "owner";
     @Autowired
     AAChannelProcessor aaChannelProcessor;
 
@@ -52,42 +62,56 @@ class AAChannelProcessorMultiIT {
         mockArchiverAppliance.shutdown();
     }
 
-    @Test
-    void testProcessMulti()
-        throws JsonProcessingException, InterruptedException {
+    static Stream<Arguments> provideArguments() {
         List<Channel> channels = List.of(
-            new Channel("PVArchivedActive", "owner", List.of(archiveProperty, activeProperty), List.of()),
-            new Channel("PVPausedActive", "owner", List.of(archiveProperty, activeProperty), List.of()),
-            new Channel("PVNoneActive0", "owner", List.of(archiveProperty, activeProperty), List.of()),
-            new Channel("PVArchivedInactive", "owner", List.of(archiveProperty, inactiveProperty), List.of()),
-            new Channel("PVPausedInactive", "owner", List.of(archiveProperty, inactiveProperty), List.of()),
-            new Channel("PVNoneInactive", "owner", List.of(archiveProperty, inactiveProperty), List.of()),
-            new Channel("PVArchivedNotag", "owner", List.of(), List.of()),
-            new Channel("PVNoneActive1", "owner", List.of(archiveProperty, activeProperty), List.of()),
-            new Channel("PVNoneActive2", "owner", List.of(archiveProperty, activeProperty), List.of())
+            new Channel("PVArchivedActive", OWNER, List.of(archiveProperty, activeProperty), List.of()),
+            new Channel("PVPausedActive", OWNER, List.of(archiveProperty, activeProperty), List.of()),
+            new Channel("PVNoneActive0", OWNER, List.of(archiveProperty, activeProperty), List.of()),
+            new Channel("PVArchivedInactive", OWNER, List.of(archiveProperty, inactiveProperty), List.of()),
+            new Channel("PVPausedInactive", OWNER, List.of(archiveProperty, inactiveProperty), List.of()),
+            new Channel("PVNoneInactive", OWNER, List.of(archiveProperty, inactiveProperty), List.of()),
+            new Channel("PVArchivedNotag", OWNER, List.of(), List.of()),
+            new Channel("PVNoneActive1", OWNER, List.of(archiveProperty, activeProperty), List.of()),
+            new Channel("PVNoneActive2", OWNER, List.of(archiveProperty, activeProperty), List.of())
         );
 
         Map<String, String> namesToStatuses = Map.of(
-            "PVArchivedActive", "Being archived",
-            "PVPausedActive", "Paused",
-            "PVNoneActive0", "Not being archived",
-            "PVArchivedInactive", "Being archived",
-            "PVPausedInactive", "Paused",
-            "PVNoneInactive", "Not being archived",
-            "PVArchivedNotag", "Being archived",
-            "PVNoneActive1", "Not being archived",
-            "PVNoneActive2", "Not being archived"
+            "PVArchivedActive", BEING_ARCHIVED,
+            "PVPausedActive", PAUSED,
+            "PVNoneActive0", NOT_BEING_ARCHIVED,
+            "PVArchivedInactive", BEING_ARCHIVED,
+            "PVPausedInactive", PAUSED,
+            "PVNoneInactive", NOT_BEING_ARCHIVED,
+            "PVArchivedNotag", BEING_ARCHIVED,
+            "PVNoneActive1", NOT_BEING_ARCHIVED,
+            "PVNoneActive2", NOT_BEING_ARCHIVED
         );
         Map<ArchiveAction, List<String>> actionsToNames = Map.of(
             ArchiveAction.RESUME, List.of("PVPausedActive"),
             ArchiveAction.PAUSE, List.of("PVArchivedInactive", "PVArchivedNotag"),
             ArchiveAction.ARCHIVE, List.of("PVNoneActive0", "PVNoneActive1", "PVNoneActive2")
         );
-        Map<ArchiveAction, List<String>> actionsToSubmissions = Map.of(
-            ArchiveAction.ARCHIVE, List.of("PVNoneActive2", "PVNoneActive1", "PVNoneActive0"),
-            ArchiveAction.RESUME, List.of("PVPausedActive"),
-            ArchiveAction.PAUSE, List.of("PVArchivedNotag", "PVArchivedInactive")
-        );
+        int expectedProcessedChannels = 6;
+
+        List<String> massPVNames = IntStream.range(1, 100).mapToObj(i -> "PV" + i).toList();
+        return Stream.of(
+            Arguments.of(channels, namesToStatuses, actionsToNames, expectedProcessedChannels),
+            Arguments.of(
+                massPVNames.stream().map(s -> new Channel(s, OWNER, List.of(archiveProperty, activeProperty), List.of())).toList(),
+                massPVNames.stream().collect(Collectors.toMap(String::toString, e -> NOT_BEING_ARCHIVED)),
+                Map.of(ArchiveAction.ARCHIVE, massPVNames),
+                massPVNames.size()
+            ));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void testProcessMulti(List<Channel> channels,
+                          Map<String, String> namesToStatuses,
+                          Map<ArchiveAction, List<String>> actionsToNames,
+                          int expectedProcessedChannels)
+        throws JsonProcessingException, InterruptedException {
+
         // Request to version
         Map<String, String> versions = Map.of("mgmt_version", "Archiver Appliance Version 1.1.0");
         mockArchiverAppliance.enqueue(new MockResponse()
@@ -121,7 +145,7 @@ class AAChannelProcessorMultiIT {
         });
 
         long count = aaChannelProcessor.process(channels);
-        assertEquals(count, 6);
+        assertEquals(count, expectedProcessedChannels);
 
         AtomicInteger expectedRequests = new AtomicInteger(1);
         RecordedRequest requestVersion = mockArchiverAppliance.takeRequest(2, TimeUnit.SECONDS);
@@ -154,7 +178,7 @@ class AAChannelProcessorMultiIT {
             assertTrue(requestAction.getPath().startsWith("/mgmt/bpl"));
             ArchiveAction key = actionFromEndpoint(requestAction.getPath().substring("/mgmt/bpl".length()));
             String body = requestAction.getBody().readUtf8();
-            actionsToSubmissions.get(key).forEach(pv ->
+            actionsToNames.get(key).forEach(pv ->
                 assertTrue(body.contains(pv))
             );
         }
