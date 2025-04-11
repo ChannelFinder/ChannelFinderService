50084326dab1fbf4cdc8664b5e94d876e65ca61f
Conor Schofield <conorschofield@lbl.gov>
2025-04-11 15:32:23 -0700

    add default archiver to test property

--- src/test/java/org/phoebus/channelfinder/processors/AAChannelProcessorMultiIT.java
+++ src/test/java/org/phoebus/channelfinder/processors/AAChannelProcessorMultiIT.java
@@ -36,7 +36,7 @@ import static org.phoebus.channelfinder.processors.AAChannelProcessorIT.archiveP
 import static org.phoebus.channelfinder.processors.AAChannelProcessorIT.inactiveProperty;
 
 @WebMvcTest(AAChannelProcessor.class)
-@TestPropertySource(value = "classpath:application_test.properties")
+@TestPropertySource(locations = "classpath:application_test.properties", properties = "aa.post_support:default")
 class AAChannelProcessorMultiIT {
 
     public static final String BEING_ARCHIVED = "Being archived";
