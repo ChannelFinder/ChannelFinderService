472fa2b00f3fe241e40416bc4db321c149ce61e1
Domonkos Gulyas <domonkos.gulyas@ess.edu>
2026-01-14 09:15:48 +0100

    Fix MetricsServiceIT REST calls

--- src/test/java/org/phoebus/channelfinder/MetricsServiceIT.java
+++ src/test/java/org/phoebus/channelfinder/MetricsServiceIT.java
@@ -89,13 +89,13 @@ class MetricsServiceIT {
   private void getAndExpectMetric(String paramValue, String endpoint, int expectedValue)
       throws Exception {
     mockMvc
-        .perform(get(METRICS_ENDPOINT + endpoint).param(METRICS_PARAM_KEY, paramValue))
+        .perform(get(METRICS_ENDPOINT + "/" + endpoint).param(METRICS_PARAM_KEY, paramValue))
         .andExpect(jsonPath("$.measurements[0].value").value(expectedValue));
   }
 
   private void getAndExpectMetricParent(String endpoint, int expectedValue) throws Exception {
     mockMvc
-        .perform(get(METRICS_ENDPOINT + endpoint))
+        .perform(get(METRICS_ENDPOINT + "/" + endpoint))
         .andExpect(jsonPath("$.measurements[0].value").value(expectedValue));
   }
 
