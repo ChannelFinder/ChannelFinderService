ac688d578060f80090bca94e03dc11eb2d6a30d7
Simon Rose <simon.rose@ess.eu>
2026-02-09 14:51:08 +0100

    Simplify SearchResult record type

--- src/main/java/org/phoebus/channelfinder/entity/SearchResult.java
+++ src/main/java/org/phoebus/channelfinder/entity/SearchResult.java
@@ -1,25 +1,5 @@
 package org.phoebus.channelfinder.entity;
 
-import com.google.common.base.Objects;
 import java.util.List;
 
-public record SearchResult(List<Channel> channels, long count) {
-
-  @Override
-  public String toString() {
-    return "SearchResult{" + "count=" + count + ", channels=" + channels + '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    SearchResult that = (SearchResult) o;
-    return count == that.count && Objects.equal(channels, that.channels);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(count, channels);
-  }
-}
+public record SearchResult(List<Channel> channels, long count) {}
